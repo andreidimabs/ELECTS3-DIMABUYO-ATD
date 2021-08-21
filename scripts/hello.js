2d986f5a0ad18dbc662ed3b13cd4358744e8472f
andreidimabs <dimabuyo.andrei@auf.edu.ph>
2021-08-21 19:38:19 +0800

    commit

---
+++ scripts/hello.js
@@ -0,0 +1,9 @@
+function msg(){
+    alert("Hello World! This is External Script");
+}
+function msg2(){
+    alert("Hello World!");
+}
+function msg3(){
+    alert("This is External Javascript!");
+}
